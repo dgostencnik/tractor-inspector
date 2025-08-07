2b699c63cd391a46ae2d98ec1c6e9151420c116b
Damjan Gostenčnik <damjan.gostencnik@gmail.com>
2025-08-07 23:06:50 +0200

    fe: add not found page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import ActivitiesLoader from "../views/activities-loader.vue";
 import ActivitiesView from "../views/activities-view.vue";
+import NotFoundView from "../views/not-found-view.vue";
 import TelemetryLogEdit from "../views/telemetry-log-edit.vue";
 import TractorView from "../views/tractor-view.vue";
 import TractorsView from "../views/tractors-view.vue";
@@ -12,6 +13,7 @@ const routes = [
   { path: "/tractors/:id/telemetry/:itemId", name: "TelemetryLogEdit", component: TelemetryLogEdit },
   { path: "/activities", name: "Fleet map activities overview", component: ActivitiesLoader },
   { path: "/activities/:date", name: "Fleet map", component: ActivitiesView },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundView },
 
 ];
 
---
+++ frontend/src/views/not-found-view.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import AppHeader from "../components/app-header.vue";
+import DisplayMessage from "../components/display-message.vue";
+</script>
+
+<template>
+  <AppHeader icon="tabler:tractor">
+    <template #title>
+      <div class="flex gap-4 items-center">
+        <h1 class="text-2xl font-bold">
+          Tractor Inspector
+        </h1>
+        <RouterLink
+          class="btn btn-primary"
+          to="/activities"
+        >
+          <Icon icon="tabler:map" />
+          <span>Fleet Map</span>
+        </RouterLink>
+      </div>
+    </template>
+  </AppHeader>
+
+  <main class="flex flex-col max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 h flex-1 w-full">
+    <DisplayMessage
+      title="Error"
+      description="Page not found"
+      icon="tabler:alert-triangle"
+      class="flex-1"
+    >
+      <div class="flex gap-2 justify-center">
+        <RouterLink to="/" class="btn btn-success">
+          Home
+        </RouterLink>
+      </div>
+    </DisplayMessage>
+  </main>
+</template>
